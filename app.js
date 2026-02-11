e817d59c75dd627ce3e36a26b48e4c22088e5a41
sebashiguita22 <sebastianhiguitau@gmail.com>
2026-02-11 11:01:42 -0500

    intento 1

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const $ = (selector) => document.querySelector(selector);
 const form = $("#form");
 const input = $("#name");
 const resetBtn = $("#resetBtn");
+$("#group").textContent = "Ingenieria Web - Quiz 1";
+$("#code").textContent = "C.C 1036451596";
 
 input.addEventListener("input", clearMessage);
 resetBtn.addEventListener("click", () => {
@@ -9,12 +11,9 @@ resetBtn.addEventListener("click", () => {
     clearMessage();
   });
 
-$("#group").textContent = "Ingenieria Web - Quiz 1";
-$("#code").textContent = "C.C 1036451596";
+
 function clearMessage() {
   const result = $("#result");
   result.textContent = "";
   result.className = "";
 }
-
-document.addEventListener("DOMContentLoaded", init);
